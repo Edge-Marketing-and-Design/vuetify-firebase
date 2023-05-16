cd34f28e9c5ffdde62f4ae9f8d5d78f6f4368e32
sethjfischer <seth@itsgeek2me.net>
2023-05-16 11:45:21 -0700

    updates

--- postinstall.sh
+++ postinstall.sh
@@ -12,9 +12,43 @@ destination_dir="$project_root/"
 # use rsync to copy files from the source to the destination
 rsync -av --ignore-existing "$source_dir" "$destination_dir"
 
-sed -i.backup '/\/\/ EDGE START/,/\/\/ EDGE END/d' "$project_root/middleware/auth.ts"
+cp "./src/composables/edgeGlobal.ts" "$project_root/composables/edgeGlobal.ts"
+cp "./src/composables/vuetify.ts" "$project_root/composables/vuetify.ts"
 
-awk '/\/\/ EDGE START/,/\/\/ EDGE END/' ./src/middleware/auth.ts | \
-  sed '1d;$d' | \
-  sed -e '1s/^/\/\/ EDGE START\n/' -e '$s/$/\n\/\/ EDGE END/' \
-  >> "$project_root/middleware/auth.js";
+
+# Update the auth middleware
+
+# Extract new block content.
+new_block=$(awk '/\/\/ EDGE START/,/\/\/ EDGE END/' ./src/middleware/auth.ts | sed '1d;$d' | sed -e '1s/^/\/\/ EDGE START\n/' -e '$s/$/\n\/\/ EDGE END/')
+
+# Create a temporary file.
+temp_file=$(mktemp)
+
+# Flag to indicate whether we are before, inside, or after the block.
+state="before"
+
+while IFS= read -r line; do
+  case $state in
+    before)
+      if [[ $line == *'// EDGE START'* ]]; then
+        state="inside"
+        # Insert new block when we reach the start of the old block.
+        echo "$new_block" >> "$temp_file"
+      else
+        echo "$line" >> "$temp_file"
+      fi
+      ;;
+    inside)
+      if [[ $line == *'// EDGE END'* ]]; then
+        state="after"
+      fi
+      # Do nothing for lines inside the old block.
+      ;;
+    after)
+      echo "$line" >> "$temp_file"
+      ;;
+  esac
+done < "$project_root/middleware/auth.ts"
+
+# Replace the original file with the new file.
+mv "$temp_file" "$project_root/middleware/auth.ts"
